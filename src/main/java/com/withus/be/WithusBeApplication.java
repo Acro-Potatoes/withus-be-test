36489b74c9f2ee5fcebd47034e1cf89e29c62b49
leewg97 <leewg97@naver.com>
2024-01-08 22:36:50 +0900

    gradle project init

---
+++ src/main/java/com/withus/be/WithusBeApplication.java
@@ -0,0 +1,13 @@
+package com.withus.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithusBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithusBeApplication.class, args);
+	}
+
+}
